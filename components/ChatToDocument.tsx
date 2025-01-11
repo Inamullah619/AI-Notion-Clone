f4eac3f315d9e5ef6e58f4a9dff76f1ce8e08895
Inamullah 619 <152864264+Inamullah619@users.noreply.github.com>
2025-01-11 19:28:05 +0500

    Publish

--- components/ChatToDocument.tsx
+++ components/ChatToDocument.tsx
@@ -2,7 +2,6 @@
 import * as Y from "yjs";
 import {
   Dialog,
-  DialogClose,
   DialogContent,
   DialogDescription,
   DialogFooter,
@@ -10,14 +9,14 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { FormEvent, use, useState, useTransition } from "react";
+// import { FormEvent, use, useState, useTransition } from "react";
 import { Button } from "./ui/button";
-import { usePathname, useRouter } from "next/navigation";
+import { FormEvent, useState, useTransition } from "react";
 import { deleteDocument, inviteUserToDocument } from "@/actions/actions";
-import { toast } from "sonner";
 import { Input } from "./ui/input";
-import { BotIcon, MessageCircleCode } from "lucide-react";
 import Markdown from "react-markdown";
+import { BotIcon, MessageCircleCode } from "lucide-react";
+import { toast } from "sonner";
 
 const ChatToDocument = ({ doc }: { doc: Y.Doc }) => {
   const [input, setInput] = useState("");
@@ -29,7 +28,36 @@ const ChatToDocument = ({ doc }: { doc: Y.Doc }) => {
   const handleAskQuestion = async (e: FormEvent) => {
     e.preventDefault();
     setQuestion(input);
+    startTransition(async () => {
+      try {
+        const documentData = doc.get("document-store").toJSON();
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/chatToDocument`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ documentData, question: input }),
+          },
+        );
+
+        if (!res.ok) {
+          throw new Error(`Server error: ${res.status}`);
+        }
+
+        const { message } = await res.json();
+
+        setInput("");
+        setSummary(message);
+        toast.success("Question Asked Successfully!");
+      } catch (err) {
+        // console.error("Error asking question:", err);
+        toast.error("Failed to ask the question. Please try again.");
+      }
+    });
   };
+
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <Button asChild variant={"outline"}>
